41b755871d68ff0c26f4af97d4484195ba0f151c
Girish Lade <girishlade111@gmail.com>
2026-07-21 11:46:06 -0700

    feat: export shared modules and types from the new index file

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,7 @@
+export * from './types/models';
+export * from './types/api';
+export * from './validators';
+export * from './constants';
+export * from './permissions';
+export * from './errors';
+export * from './utils';
